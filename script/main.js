89c04679ed6936a6f9e4fcf18c99f50c526673aa
Leoozz <leozeraffcanal2405@gmail.com>
2025-09-14 00:28:10 -0300

    Adcicionando-Funcionalidades

--- script/main.js
+++ script/main.js
@@ -1,9 +1,7 @@
 var btnTurmas = document.querySelector('#btn-turmas');
-btnTurmas === null || btnTurmas === void 0 ? void 0 : btnTurmas.addEventListener('click', function () {
-});
+btnTurmas === null || btnTurmas === void 0 ? void 0 : btnTurmas.addEventListener('click', renderTurmas);
 var btnAula = document.querySelector('#btn-aula');
-btnAula.addEventListener('click', function () {
-});
+btnAula.addEventListener('click', renderAula);
 var btnUpload = document.querySelector('#btn-upload');
 btnUpload.addEventListener('click', function () {
 });
@@ -16,4 +14,13 @@ function attConteudoarea(html) {
         areaConteudo.innerHTML = html;
     }
     ;
+}
+function renderTurmas() {
+    var html = "\n    <h2 class=\"text-2xl font-bold mb-4\">Cadastro de Turmas e Alunos</h2>\n    <form class=\"space-y-4\">\n      <input type=\"text\" placeholder=\"Nome da turma\" class=\"border p-2 w-full rounded\" />\n      <input type=\"text\" placeholder=\"Nome do aluno\" class=\"border p-2 w-full rounded\" />\n      <button type=\"submit\" class=\"bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600\">Salvar</button>\n    </form>\n  ";
+    attConteudoarea(html);
+}
+;
+function renderAula() {
+    var html = "\n        <h2 class=\"text-2xl font-bold mb-4 \">Registro Aula</h2>\n        <form action=\"\" class=\"space-y-4\">\n          <textarea name=\"\" id=\"\" class=\"border p-2 w-full rounded\" placeholder=\"Anotacoes\"></textarea>\n          <input type=\"text\" class=\"border p-2 w-full rounded \"placeholder=\"Qual sua turma\">\n          <input type=\"date\" class=\"border p-2 rounded\">\n          <button type=\"submit\"class=\"bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600\">Envio</button>\n        </form>\n    ";
+    attConteudoarea(html);
 }
--- script/main.ts
+++ script/main.ts
@@ -1,13 +1,9 @@
-const btnTurmas = document.querySelector('#btn-turmas') as HTMLButtonElement;
 
-btnTurmas?.addEventListener('click', () => {
-  
-});
+const btnTurmas = document.querySelector('#btn-turmas') as HTMLButtonElement;
+btnTurmas?.addEventListener('click',renderTurmas);
 
 const btnAula=document.querySelector('#btn-aula') as HTMLButtonElement;
-btnAula.addEventListener('click',()=>{
-    
-});
+btnAula.addEventListener('click',renderAula);
 
 const btnUpload=document.querySelector('#btn-upload') as HTMLButtonElement;
 btnUpload.addEventListener('click',()=>{
@@ -25,4 +21,29 @@ function attConteudoarea(html:string){
    if(areaConteudo){
     areaConteudo.innerHTML= html
 };
+}
+
+function renderTurmas() {
+  const html = `
+    <h2 class="text-2xl font-bold mb-4">Cadastro de Turmas e Alunos</h2>
+    <form class="space-y-4">
+      <input type="text" placeholder="Nome da turma" class="border p-2 w-full rounded" />
+      <input type="text" placeholder="Nome do aluno" class="border p-2 w-full rounded" />
+      <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Salvar</button>
+    </form>
+  `;
+  attConteudoarea(html);
+};
+
+function renderAula(){
+    const html=`
+        <h2 class="text-2xl font-bold mb-4 ">Registro Aula</h2>
+        <form action="" class="space-y-4">
+          <textarea name="" id="" class="border p-2 w-full rounded" placeholder="Anotacoes"></textarea>
+          <input type="text" class="border p-2 w-full rounded "placeholder="Qual sua turma">
+          <input type="date" class="border p-2 rounded">
+          <button type="submit"class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Envio</button>
+        </form>
+    `;
+    attConteudoarea(html)
 }
